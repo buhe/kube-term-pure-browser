d70d77d2dd39ad47fad903d6cf627a1c2a48c440
buhe <bugu1986@126.com>
2022-12-28 11:29:42 +0800

    git-u:update

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ReactTerminal } from "react-terminal";
+import { TerminalContextProvider } from "react-terminal";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <TerminalContextProvider>
+            <ReactTerminal
+              commands={{}}
+              defaultHandler={(command: any, commandArguments: any) => {
+                return `${command} us ws`;
+              }}
+            />
+          </TerminalContextProvider>
+          
+        
     </div>
   );
 }
