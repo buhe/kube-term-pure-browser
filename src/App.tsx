59a1e5e4518938c2eea4fde7c773192007ddf02a
晒晕的兔子 <gubu1986@126.com>
2022-12-28 15:08:24 +0800

    not has compile error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import { ReactTerminal } from "react-terminal";
 import { TerminalContextProvider } from "react-terminal";
-
+import { Exec } from './kube/exec';
+import { KubeConfig } from './kube/config';
+const kc = new KubeConfig();
+kc.loadFromDefault();
+let exec = new Exec(kc)
 
 function App() {
   return (
@@ -11,8 +15,20 @@ function App() {
           <TerminalContextProvider>
             <ReactTerminal
               commands={{}}
-              defaultHandler={(command: any, commandArguments: any) => {
-                return `${command} us ws`;
+              showControlBar={false}
+              theme={"dark"}
+              defaultHandler={async (command: any, commandArguments: any) => {
+                // return `${command} ${commandArguments} via ws`;
+                await exec.exec('default', 'nginx-4217019353-9gl4s', 'nginx', 'sh', `${command} ${commandArguments}`, (output) => {
+                  // output
+                  
+                 }, true /* tty */,
+                  (status: any) => {
+                    // tslint:disable-next-line:no-console
+                    console.log('Exited with status:');
+                    // tslint:disable-next-line:no-console
+                    console.log(JSON.stringify(status, null, 2));
+                  })
               }}
             />
           </TerminalContextProvider>
